8a2d348fb6ad1b643b0ce45cdfcf5e428e4715d7
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-22 17:48:32 +0100

    eliminado import no usado

--- src/main/java/com/example/pizzeria/model/Pizza.java
+++ src/main/java/com/example/pizzeria/model/Pizza.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;

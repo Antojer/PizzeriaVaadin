24aa6d561e2bf3727219b7738a0fe44902413d1f
arobles <arobles@atdev>
2018-02-22 16:37:45 +0100

    Ingredient model

--- src/main/java/com/example/pizzeria/model/Ingredient.java
+++ src/main/java/com/example/pizzeria/model/Ingredient.java
@@ -1,5 +1,28 @@
 package com.example.pizzeria.model;
 
-public class Ingredient {
+import java.awt.List;
+import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Document
+@Getter
+@Setter
+public class Ingredient implements Serializable {
+	
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+	
+	private String nombre;
+	
+	private List<Pizza> pizzas = new ArrayList<>();
+	
 }

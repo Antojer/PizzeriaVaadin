5722607f3c05b7c0d7bb0fa7189d06e65ec820a9
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-23 12:00:57 +0100

    vaadin content con dos tablas

--- src/main/java/com/example/pizzeria/VaadinUI.java
+++ src/main/java/com/example/pizzeria/VaadinUI.java
@@ -1,9 +1,13 @@
 package com.example.pizzeria;
 
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.pizzeria.dao.IngredientDAO;
+import com.example.pizzeria.dao.PizzaDAO;
 import com.example.pizzeria.model.Ingredient;
+import com.example.pizzeria.model.Pizza;
 import com.example.pizzeria.service.ingredient.IngredientService;
 import com.vaadin.annotations.Theme;
 import com.vaadin.icons.VaadinIcons;
@@ -15,18 +19,24 @@ import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 @SpringUI
 @Theme("valo")
-public class VaadinUI extends UI {
+public class VaadinUI extends UI{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Autowired
 	IngredientDAO ingredientDao;
+	
+	@Autowired
+	PizzaDAO pizzaDao;
 
 	Grid<Ingredient> gridIngredient = new Grid<>();
+	
+	Grid<Pizza> gridPizza = new Grid<>();
 	
 	@Autowired
 	IngredientService ingredientService;
@@ -36,23 +46,31 @@ public class VaadinUI extends UI {
 	private Button addNew = new Button("", this::add);
 	
 
-	
-	
 
 	@Override
 	protected void init(VaadinRequest request) {
-		setContent(new Label("Bienvenido a Pizzería Borrego!"));		
-		setContent(gridIngredient);
+		
+		VerticalLayout content = new VerticalLayout();
+		setContent(content);
+		content.addComponent(new Label("Bienvenido a Pizzería Borrego!"));
+		content.addComponent(gridIngredient);
+		content.addComponent(gridPizza);
 		
 		gridIngredient.addColumn(ingredient -> ingredient.getId()).setCaption("Ingredient ID");
 		gridIngredient.addColumn(ingredient -> ingredient.getName()).setCaption("Name");
-	
-		gridIngredient.setWidth("90%");
+		
+		refresh.setIcon(VaadinIcons.REFRESH);
+		refresh.addStyleName(ValoTheme.BUTTON_PRIMARY);
+		
+		gridPizza.addColumn(ingredient -> ingredient.getId()).setCaption("Pizza ID");
+		gridPizza.addColumn(ingredient -> ingredient.getName()).setCaption("Name");
+		gridPizza.addColumn(ingredient -> ingredient.getIngredients()).setCaption("Ingredients");
 		
 		refresh.setIcon(VaadinIcons.REFRESH);
 		refresh.addStyleName(ValoTheme.BUTTON_PRIMARY);
 		
 		listIngredients();
+		listPizzas();
 		
 	}
 	
@@ -62,6 +80,10 @@ public class VaadinUI extends UI {
 	
 	private void listIngredients() {
 		gridIngredient.setItems(ingredientDao.findAll());
+	}
+	
+	private void listPizzas() {
+		gridPizza.setItems(pizzaDao.findAll());
 	}
 	
 	private void add(ClickEvent clickEvent){
